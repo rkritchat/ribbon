bb14e3f29e4599fc86c7e88412a32846cb1c4853
rkritchat <rkritchat@gmail.com>
2020-01-17 10:44:53 +0700

    move application to docker and adding to compose

--- application/src/main/java/com/example/application/proxy/CalculatorProxy.java
+++ application/src/main/java/com/example/application/proxy/CalculatorProxy.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "calculator-service")
-@RibbonClient(name = "calculator-service")
+@FeignClient(name = "calculate-sv")
+@RibbonClient(name = "calculate-sv")
 public interface CalculatorProxy {
 
     @PostMapping("/calculate/plus")
